8f50d3ed5cd5c8611f4d828031c911557a11e86a
Dedulkaa <55138703+Dedulkaa@users.noreply.github.com>
2019-09-20 11:43:04 +0300

    Add methord

--- Sandbox/src/main/java/ru/stqa/homework/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/homework/sandbox/MyFirstProgram.java
@@ -5,10 +5,8 @@ public class MyFirstProgram {
   public static void main(String[] args){
 
     Point d = new Point (0,1);
-    System.out.println("Destination between " + d.p1 +" and " + d.p2 + " = " + area(d));
+    System.out.println("Destination between " + d.p1 +" and " + d.p2 + " = " + d.area());
   }
-  public static double area(Point d) {
-    return Math.sqrt((d.p2 )*(d.p2)-(d.p1)*(d.p1));
-  }
+
 
 }
--- Sandbox/src/main/java/ru/stqa/homework/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/homework/sandbox/Point.java
@@ -1,5 +1,7 @@
 package ru.stqa.homework.sandbox;
 
+import static java.lang.Math.sqrt;
+
 public class Point {
   public double p1;
   public double p2;
@@ -8,5 +10,7 @@ public class Point {
     this.p1 = p1;
     this.p2 = p2;
   }
-
+  public double area() {
+    return sqrt((this.p1)*(this.p2)-(this.p1)*(this.p1));
+  }
 }
