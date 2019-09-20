f9b28bdfb0b92439b4f401a6cb725b9399baea47
Dedulkaa <55138703+Dedulkaa@users.noreply.github.com>
2019-09-20 11:30:06 +0300

    Create Class and object

--- Sandbox/src/main/java/ru/stqa/homework/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/homework/sandbox/MyFirstProgram.java
@@ -4,7 +4,11 @@ public class MyFirstProgram {
 
   public static void main(String[] args){
 
-    System.out.println("Hello, world!!");
-}
+    Point d = new Point (0,1);
+    System.out.println("Destination between " + d.p1 +" and " + d.p2 + " = " + area(d));
+  }
+  public static double area(Point d) {
+    return Math.sqrt((d.p2 )*(d.p2)-(d.p1)*(d.p1));
+  }
 
 }
